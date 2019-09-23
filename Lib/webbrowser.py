f55317a2a10d85c3240225d3c22f76e155cbe911
Philipp Wolfer <phil@parolu.io>
2019-09-23 09:14:58 +0200

    webbrowser: Support for default browsers on Haiku

--- Lib/webbrowser.py
+++ Lib/webbrowser.py
@@ -527,6 +527,11 @@ def register_standard_browsers():
                         "netscape", "opera", iexplore):
             if shutil.which(browser):
                 register(browser, None, BackgroundBrowser(browser))
+    elif sys.platform[:5] == "haiku":
+        # First try to use the default configured browser
+        register("haiku-default", None, GenericBrowser("open"))
+        # Fall back to WebPositive as the standard browser of Haiku
+        register("webpositive", None, BackgroundBrowser("WebPositive"))
     else:
         # Prefer X browsers if present
         if os.environ.get("DISPLAY"):
